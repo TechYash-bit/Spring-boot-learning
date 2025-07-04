914fe0cf03164414c128a647e0244cbe3b6e81a9
TechYash-bit <TechYash-bit@users.noreply.github.com>
2025-07-04 07:17:09 +0530

    add account and retrive all accounts

--- BankingSystem/src/main/java/com/techYash_bit/BankingSystem/Controller/ManagerBankController.java
+++ BankingSystem/src/main/java/com/techYash_bit/BankingSystem/Controller/ManagerBankController.java
@@ -2,23 +2,31 @@ package com.techYash_bit.BankingSystem.Controller;
 
 import com.techYash_bit.BankingSystem.Dto.AccountDto;
 import com.techYash_bit.BankingSystem.Services.AccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/account")
-public class Accounts {
+public class ManagerBankController {
 
     final private AccountService accountService;
 
-    Accounts(AccountService accountService){
+    ManagerBankController(AccountService accountService){
         this.accountService=accountService;
     }
 
     @GetMapping("/{accno}")
     public AccountDto getAccount(@PathVariable int accno){
         return accountService.findAccount(accno);
+    }
+    @PostMapping("/addAcc")
+
+    public AccountDto addAccoutn(@RequestBody AccountDto accountDto){
+        return accountService.addAccount(accountDto);
+    }
+    @GetMapping("/findAll")
+    public List<AccountDto> findAll(){
+        return accountService.findAllRecord();
     }
 }
--- BankingSystem/src/main/java/com/techYash_bit/BankingSystem/Services/AccountService.java
+++ BankingSystem/src/main/java/com/techYash_bit/BankingSystem/Services/AccountService.java
@@ -6,6 +6,9 @@ import com.techYash_bit.BankingSystem.Repositories.Accountrepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AccountService {
 
@@ -18,5 +21,16 @@ public class AccountService {
     public AccountDto findAccount(int accno){
         AccountEntity accountEntity=accountrepo.findById(accno).orElse(null);
         return modelMapper.map(accountEntity,AccountDto.class);
+    }
+    public AccountDto addAccount(AccountDto accountDto){
+       AccountEntity accountEntity=modelMapper.map(accountDto,AccountEntity.class);
+       accountrepo.save(accountEntity);
+       return modelMapper.map(accountEntity,AccountDto.class);
+    }
+    public List<AccountDto> findAllRecord(){
+        List<AccountEntity> accountEntity=accountrepo.findAll();
+        return accountEntity.stream()
+                .map(entity-> modelMapper.map(entity,AccountDto.class))
+                .collect(Collectors.toList());
     }
 }
